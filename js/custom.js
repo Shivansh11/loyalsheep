defa7426c495dc4b9269f5c0167f80abf296d4c8
Shivansh Agrawal <shivansh11@outlook.com>
2019-04-03 12:31:41 +0530

    New bootstrap Website

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+// Offset for Site Navigation
+$('#siteNav').affix({
+	offset: {
+		top: 100
+	}
+})
+
+$(function() {
+	$(document).scroll(function() {
+		var $nav = $("nav");
+		$nav.toggleClass('navScrolled', $(this).scrollTop() > $nav.height());
+	});
+});
+
+$(function() {
+	$('li').click(function() {
+		$('.in').toggle();
+	});
+});
